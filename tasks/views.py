aec9ae51212ed2f8a65da1fd4fa86686edf2c479
Michal <michalborowiec@op.pl>
2021-04-14 12:26:29 +0200

    task1 hotfix

--- tasks/views.py
+++ tasks/views.py
@@ -11,8 +11,8 @@ class Task1View(APIView):
     def post(self, request):
         print(request.data['data_list'])
         response_dict = {'result': []}
-        for item in sorted(request.data['data_list'], key=itemgetter('second_name', 'first_name')):
-            try:
+        try:
+            for item in sorted(request.data['data_list'], key=itemgetter('second_name', 'first_name')):
                 first_name = item['first_name']
                 second_name = item['second_name']
                 birth_date = item['birth_date']
@@ -22,7 +22,7 @@ class Task1View(APIView):
                 response_dict['result'].append(
                     {'first_name': first_name, 'second_name': second_name, 'birth_date': birth_date, 'hash': hash})
 
-            except KeyError:
-                return Response(status=status.HTTP_400_BAD_REQUEST)
+        except KeyError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
         return Response(data=response_dict, status=status.HTTP_200_OK)
